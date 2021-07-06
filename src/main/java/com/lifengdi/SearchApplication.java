4c97c157ccce181d8885bc0a7d4779ef550ee808
zhouhe <zhouhe@do-global.com>
2021-07-06 12:33:36 +0800

    java

---
+++ src/main/java/com/lifengdi/SearchApplication.java
@@ -0,0 +1,20 @@
+package com.lifengdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableAsync
+@EnableConfigurationProperties
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+
+}
